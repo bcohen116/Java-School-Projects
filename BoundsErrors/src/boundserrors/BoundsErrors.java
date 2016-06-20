c650a3eadc0de438c35686d22b95c4adb1451e41
Ben Cohen <benfish116@yahoo.com>
2016-06-20 13:11:06 -0400

    Add files via upload

---
+++ BoundsErrors/src/boundserrors/BoundsErrors.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boundserrors;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ *
+ * @author Ben
+ */
+public class BoundsErrors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //contains a bounds error
+//        double[] data = new double[10];
+//        data[10] = 29.95;
+        
+        //loop that reads 10 strings and puts them into an array list.
+//        Scanner input = new Scanner(System.in);
+//        ArrayList<String> newArray = new ArrayList<String>();
+//        for (int i = 0; i < 10; i++)
+//        {
+//            System.out.println("Please enter string # " + (i + 1));
+//            newArray.add(input.next());
+//            System.out.println("String " + (i + 1) + " is: " + newArray.get(i));
+//        }
+        
+        
+        //loop that does the previos loop backwards
+//        Scanner input = new Scanner(System.in);
+//        ArrayList<String> newArray = new ArrayList<String>();
+//        for (int i = 0; i < 10; i++)
+//        {
+//            System.out.println("Please enter string # " + (i + 1));
+//            newArray.add(input.next());
+//        }
+//        for (int j = 0; j < 10; j++)
+//        {
+//            System.out.println("String " + (j + 1) + " is: " + newArray.get(9 - j));
+//        }
+        
+        
+        //loop that puts 10 random numbers into an array
+//        int[] a = new int[10];
+//        int[] b = new int[10];
+//        Random randomNumber = new Random();
+//        for (int i = 0; i < 10; i++)
+//        {    
+//            a[i] = randomNumber.nextInt(100);
+//            b[i] = a[i];
+//            System.out.println(a[i]);
+//        }  
+//        System.out.println("\n");
+//        for (int i = 0; i < 10; i++)
+//        {
+//            
+//            a[i] = randomNumber.nextInt(100);
+//            if (a[i] == b[i])
+//            {
+//                i--;
+//                continue;
+//            }
+//            System.out.println(a[i]);
+//        }  
+        
+        
+        //make an array that holds 20 numbers and make the first 10 hold 1,4,9,...,100.
+//        int[] a = new int[20];
+//        for (int i = 0; i < 20; i++)
+//        {
+//            if (i < 10)
+//            {
+//            a[i] = (i * 5) - 1;
+//            if (a[i] < 0)
+//                a[i] = 1;
+//            }
+//            System.out.println(a[i]);
+//        }
+    }      
+}
